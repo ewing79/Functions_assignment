c2093eeba1117f51cd12e50993d3ed4583660d2a
Shaylen Young <young777@fullsail.edu>
2013-06-27 21:34:48 -0500

    set up correction

--- Industry/js/industry.js
+++ Industry/js/industry.js
@@ -4,7 +4,11 @@
 //Cost for everything
 var money = 40
 var ticketsPer = 10
+var people = 2
 var theaterfood = 20
 var extrafor3D = 4
 var redbox = 1
 var pizza = 12
+
+//Total for Theater
+var theaterTotal =
