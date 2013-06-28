4f37515bff5bd5b93d7fcc0ac0bd26a750eb26f9
Shaylen Young <young777@fullsail.edu>
2013-06-27 21:39:53 -0500

    home movie and theater

--- Industry/js/industry.js
+++ Industry/js/industry.js
@@ -8,8 +8,9 @@
     var theaterfood = 20;
     var extrafor3D = 4;
     var redbox = 1.50;
-var pizza = 12
+    var pizza = 12
 
-//Total for Theater
-var theaterTotal = ((ticketsPer * people) + theaterfood );
-//
+    //Total for Theater
+    var theaterTotal = ((ticketsPer * people) + theaterfood );
+    //Total for Home Movie Night
+    var homeMovie = ((redbox * 2) + pizza)
