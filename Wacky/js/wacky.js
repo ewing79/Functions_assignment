a3cdf876221397dae47d4698b937426fe58e9b42
Shaylen Young <young777@fullsail.edu>
2013-06-27 22:47:09 -0500

    wacky set up calclation

--- Wacky/js/wacky.js
+++ Wacky/js/wacky.js
@@ -1,3 +1,5 @@
 //Shaylen Young - 06/20 - Wacky
 
 //Cost for ET to Phone Home
+//Et has a special rate plan to where he pays .20 per minute for long distance calls
+//it is an extra .50 per minute for any call over 100 miles away. Mars is 228.3 million miles away
