41ab6cd43ad23264be3f2cf2c2e553f4fc796c8e
Shaylen Young <young777@fullsail.edu>
2013-06-27 21:36:57 -0500

    set up total correction

--- Industry/js/industry.js
+++ Industry/js/industry.js
@@ -1,14 +1,15 @@
 //Shaylen Young - 06/21 - Movie Night
 
 //Theater or Redbox?
-//Cost for everything
-var money = 40
-var ticketsPer = 10
-var people = 2
-var theaterfood = 20
-var extrafor3D = 4
-var redbox = 1
+    //Cost for everything
+    var money = 40;
+    var ticketsPer = 10;
+    var people = 2;
+    var theaterfood = 20;
+    var extrafor3D = 4;
+    var redbox = 1.50;
 var pizza = 12
 
 //Total for Theater
-var theaterTotal =
+var theaterTotal = ((ticketsPer * people) + theaterfood );
+//
