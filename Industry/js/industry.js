5656ed2f9a90344d6154451569d6e7302716de73
Shaylen Young <young777@fullsail.edu>
2013-06-27 21:33:06 -0500

    personal cost

--- Industry/js/industry.js
+++ Industry/js/industry.js
@@ -1,6 +1,10 @@
 //Shaylen Young - 06/21 - Movie Night
 
 //Theater or Redbox?
-
-
-
+//Cost for everything
+var money = 40
+var ticketsPer = 10
+var theaterfood = 20
+var extrafor3D = 4
+var redbox = 1
+var pizza = 12
