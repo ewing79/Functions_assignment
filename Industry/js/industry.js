d5164a1df20581bff2a182a6b8d9cbcefb7fb589
Shaylen Young <young777@fullsail.edu>
2013-06-27 22:13:36 -0500

    industry set up

--- Industry/js/industry.js
+++ Industry/js/industry.js
@@ -1,7 +1,1 @@
-/**
- * Created with JetBrains WebStorm.
- * User: Young
- * Date: 6/27/13
- * Time: 10:01 PM
- * To change this template use File | Settings | File Templates.
- */
+//Shaylen Young - 06/21 - Industry
