d8d828a6068b161e8df87f8a63f9076a659f6103
Shaylen Young <young777@fullsail.edu>
2013-06-27 22:03:53 -0500

    personal name change correction

--- Industry/js/industry.js
+++ Industry/js/industry.js
@@ -1,33 +1,7 @@
-//Shaylen Young - 06/21 - Movie Night
-
-//Theater or Redbox?
-    //Cost for everything
-    var money = 40;
-    var ticketsPer = 10;
-    var people = 2;
-    var theaterfood = 20;
-    var extrafor3D = 4;
-    var redbox = 1.50;
-    var pizza = 12
-
-    //Total for Theater
-    var theaterTotal = calculation
-
-    function calculation(ticketsPer, people, theaterfood){
-        var theaterCost = ticketsPer * people + theaterfood;
-        return theaterCost
-    }
-    //if Theater cost more than the money we have then we cannot go to the movies.
-if(theaterCost > money){
-    //If we have more money than theater cost then we can go to the movies
-    console.log("Let's go to the Movies");
-}
-
-    //Total for Home Movie Night
-    var homeMovie = calculation2
-
-    function calculation2(redbox, pizza){
-        var homeMovietotal = redbox * 2 + pizza
-        return homeMovietotal
-}else{
-    console.log("Lets stay in and order a movie")}
+/**
+ * Created with JetBrains WebStorm.
+ * User: Young
+ * Date: 6/27/13
+ * Time: 10:01 PM
+ * To change this template use File | Settings | File Templates.
+ */
