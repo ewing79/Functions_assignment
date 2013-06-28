6d115accc835ba279b5565cf533213390c117d94
Shaylen Young <young777@fullsail.edu>
2013-06-27 22:22:17 -0500

    industry set up and calculate

--- Industry/js/industry.js
+++ Industry/js/industry.js
@@ -1,1 +1,13 @@
 //Shaylen Young - 06/21 - Industry Assignment
+
+//Freelance - Cost for small business website
+
+var Chargetotal = businessNeeds(50, 30, 40, 60, 10)
+//business needs
+
+function businessNeeds(html5, java, css, video, updates){
+    //
+    var totalNeeds = html5 + java + css + video + updates
+    return totalNeeds
+}
+console.log("It will cost " + Chargetotal + " to make your website")
