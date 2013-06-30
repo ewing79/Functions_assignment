d807426ba54238a297cc42c5ee6b6a5c8bcbb59f
josephdalton <jdalton@fullsail.com>
2013-06-30 15:59:00 -0400

    Update personal.js
    There are several issues with this code. I will enumerate each one below.
    Issue 1: You seem to be having issues with the idea of variable scope inside and outside of functions. The code as written would not run.
    Issue 2: You either placed a function within an if statement or you added an else to the end of a function. In either case this will not work. It looks like you were working on adding a second option but didn't get it finished.

--- Personal/js/personal.js
+++ Personal/js/personal.js
@@ -11,8 +11,9 @@
     var pizza = 12
 
     //Total for Theater
-    var theaterTotal = calculation
-
+    //Issue 1
+    var theaterTotal = calculation; 
+// CORRECTED CODE   var theaterTotal = calculation (ticketsPer,people,theaterfood);
     function calculation(ticketsPer, people, theaterfood){
         var theaterCost = ticketsPer * people + theaterfood;
         return theaterCost
@@ -20,16 +21,18 @@
     //if Theater cost more than the money we have then we cannot go to the movies.
 if(theaterCost > money){
     //If we have more money than theater cost then we can go to the movies
-    console.log("Let's go to the Movies");
+    console.log("Let's go to the Movies"); //There is a logical error in your description, if the cost is greater then the money we have then "goto the movies"
 }
-
+/******ISSUE 2: This code doesn't belong here*******/
     //Total for Home Movie Night
-    var homeMovie = calculation2
+    var homeMovie = calculation2(redbox,pizza)
 
     function calculation2(redbox, pizza){
         var homeMovietotal = redbox * 2 + pizza
         return homeMovietotal
     // If we don't have enough money it will be a movie night
+/******END: This code doesn't belong here*******/
+
 }else{
     console.log("Lets stay in and order a movie")
 }
